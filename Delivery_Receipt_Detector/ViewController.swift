84d8dcbf5b8363fbbf8cad5669485d00a655ff49
Himanshu Joshi <himanshu.joshi@roro.io>
2025-03-20 11:57:20 +0530

    Initial Commit

---
+++ Delivery_Receipt_Detector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery_Receipt_Detector
+//
+//  Created by Himanshu Joshi on 20/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
